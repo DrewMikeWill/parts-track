228605906a37f750671f8967323eb437567af30b
DrewMikeWill <drewmikewill@engineer.com>
2024-11-02 13:56:46 -0400

    Add Main and App Settings

---
+++ PartsTrack/PartsTrack/Program.cs
@@ -0,0 +1,10 @@
+﻿using PartsTrack.Application;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to PartsTrack Inventory Console App!");
+        // Initialize application services and handle user inputs here
+    }
+}
