ab3871d781531791bdd2fb38d24815e66d5785af
DrewMikeWill <drewmikewill@engineer.com>
2024-11-02 13:57:17 -0400

    Add Inventory Service

---
+++ PartsTrack/PartsTrack.Domain/Exceptions/InventoryNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PartsTrack.Domain.Exceptions
+{
+    public class InventoryNotFoundException : Exception
+    {
+        public InventoryNotFoundException(int partId, int warehouseId)
+            : base($"Inventory entry not found for Part ID {partId} in Warehouse ID {warehouseId}.")
+        {
+        }
+    }
+}
---
+++ PartsTrack/PartsTrack.Domain/Services/InventoryService.cs
@@ -0,0 +1,34 @@
+﻿using PartsTrack.Domain.Catalogs;
+using PartsTrack.Domain.Entities;
+
+namespace PartsTrack.Domain.Services
+{
+    public class InventoryService
+    {
+        private readonly IPartCatalog _partCatalog;
+        private readonly IInventoryCatalog _inventoryCatalog;
+
+        public InventoryService(IPartCatalog partCatalog, IInventoryCatalog inventoryCatalog)
+        {
+            _partCatalog = partCatalog;
+            _inventoryCatalog = inventoryCatalog;
+        }
+
+        public void AddPart(Part part)
+        {
+            _partCatalog.Save(part);
+        }
+
+        public void UpdateStock(int partId, int quantity)
+        {
+            var part = _partCatalog.GetById(partId);
+            part.UpdateStock(quantity);
+            _partCatalog.Update(part);
+        }
+
+        public Inventory GetInventory(int partId, int warehouseId)
+        {
+            return _inventoryCatalog.GetInventory(partId, warehouseId);
+        }
+    }
+}
